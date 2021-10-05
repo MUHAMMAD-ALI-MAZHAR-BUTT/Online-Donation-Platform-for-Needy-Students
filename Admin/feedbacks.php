bfe8eb942762b0302f722e9300058fcf490acc30
Abdullah Rasheed <90464608+abdullahrasheed837@users.noreply.github.com>
2021-10-05 22:42:05 +0500

    Delete Admin directory

--- Admin/feedbacks.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('./content/header.php');
-include('./content/navbar.php');
-include('./content/sidebar.php');
-include('./content/feedbacks.php');
-include('./content/footer.php');
--- Admin/news.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('./content/header.php');
-include('./content/navbar.php');
-include('./content/sidebar.php');
-include('./content/news.php');
-include('./content/footer.php');
